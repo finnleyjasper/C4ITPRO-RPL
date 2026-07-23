5f7fb13eb7745b2cbef08770d515cb508170f91e
Finnley Newnham <162288636+finnleyjasper@users.noreply.github.com>
2026-07-23 12:45:14 +1000

    Began session 1 on JS
    Updated docs for DDP

--- Data Driven Programming/Session1/obj/Debug/net10.0/Session1.AssemblyInfo.cs
+++ Data Driven Programming/Session1/obj/Debug/net10.0/Session1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Session1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7dda7b459fba55dffd0d1d9717c08b707d4bb10c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0dbb341abef633a3dbaaadef8d7bf637b6d01c7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Session1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Session1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Data Driven Programming/TodoApp/obj/Debug/net10.0/TodoApp.AssemblyInfo.cs
+++ Data Driven Programming/TodoApp/obj/Debug/net10.0/TodoApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7dda7b459fba55dffd0d1d9717c08b707d4bb10c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c0dbb341abef633a3dbaaadef8d7bf637b6d01c7")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
